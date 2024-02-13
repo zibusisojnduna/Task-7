55078beedb7275328bd479adc9bc45646ea534b4
User <User@WRSDLD309-023aa.dut.ac.za>
2024-02-13 14:03:15 +0200

    New Changes

---
+++ SRC/script.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    const main1 = document.getElementById("main1")
+    const main3 = document.getElementById("main3")
+
+    main1.style.display = "none"
+    main3.style.display = "block"
+    
+   
+}, 3000)
+
+
---
+++ script.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+    const main1 = document.getElementById("main1")
+    const main2 = document.getElementById("main2")
+
+    main1.style.display = "none"
+    main2.style.display = "block"
+    
+   
+}, 3000)
+
+
+
+//function response(){
+//document.getElementsByClassName("main2").style.display = "block"	
+//}
