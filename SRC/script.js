eeedd8c2a74d9b3cace2deda5470ec637f6dd854
User <User@WRSDLD309-023aa.dut.ac.za>
2024-02-14 13:54:44 +0200

    New Changes

--- SRC/script.js
+++ SRC/script.js
@@ -1,11 +1,35 @@
 setTimeout(function(){
     const main1 = document.getElementById("main1")
     const main3 = document.getElementById("main3")
+    const main4 = document.getElementById("main4")
 
     main1.style.display = "none"
     main3.style.display = "block"
+    main4.style.display = "block"
     
    
 }, 3000)
 
+//const url ="https://formspree.io/f/mwkgoovd"
+    // const url ="https://formspree.io/f/mdoqkynl"
 
+    //let send =JSON.stringify(guest1)
+
+   // fetch(url,{
+       /// method: "POST",
+       /// body: send,
+       // headers:{
+            ///"Content-Type":"application/json"
+       // }
+  //  })
+//.catch(function(e){console.log(e)})
+
+setTimeout(function(){
+    const main1 = document.getElementById("main1")
+    const main4 = document.getElementById("main4")
+
+    main1.style.display = "none"
+    main4.style.display = "block"
+    
+   
+}, 3000)
